33626791893e2653b854edea28c2b88e53307a5f
kimiko tani <kimitaniko@gmail.com>
2025-07-09 00:59:47 +0900

    登録画面バリデーションの修正

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -16,15 +16,8 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             'name' => ['required', 'string', 'max:20'],
             'email' => ['required', 'string','email', 'max:255', 'unique:users'],
-            'password' => ['required' ,'string', 'min:8', 'confirmed'],
-        ], [
-            'name.required' => 'お名前を入力してください',
-            'name.max' => 'お名前は20文字以内で入力してください',
-            'email.required' => 'メールアドレスを入力してください',
-            'email.email' => 'メールアドレスはメール形式で入力してください',
-            'password.required' => 'パスワードを入力してください',
-            'password.min' => 'パスワードは8文字以上で入力してください',
-            'password.confirmed' => 'パスワードと一致しません',
+            'password' => ['required' ,'string', 'min:8'],
+            'password_confirmation' => ['required' ,'string', 'min:8', 'same:password'],
         ])->validate(); //ここでチェックOKでない場合は止まってエラーを返す
 
         // 2. Userモデルを使ってデータベースに保存
